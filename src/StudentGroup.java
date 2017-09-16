2895129f47f46698d571fa0d32a432f77efbf3dd
vamshisoftware <32016711+vamshisoftware@users.noreply.github.com>
2017-09-16 20:13:52 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -48,12 +48,8 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	    public void setStudents(Student[] students) {
 		try{
-        this.students=new Student[students.length];
-        for(int i=0;i<students.length;i++)
-        {
-        this.students[i]=students[i];
-        }
-        }
+                    this.students=students;
+                   }
           	catch(IllegalArgumentException ex)
                    {
                    System.out.println(ex);
